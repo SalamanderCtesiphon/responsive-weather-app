fe90f94aab8bc51557111f27f5c4c8b7c571b454
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-05 17:17:28 -0500

    completed the logic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import hotBg from './assest/hot.jpg'
 import coldBg from './assest/cold.jpg'
 import Descriptions from './components/Descriptions';
+import { getFormattedWeatherData } from './weatherService';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [city, setCity] = useState("Paris")
+  const [weather, setWeather] = useState(null)
+  const [units, setUnits] = useState("imperial")
+  const [bg, setBg] = useState(hotBg)
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units)
+      setWeather(data)
+
+      const threshold = units === 'metric' ? 20 : 60
+      if(data.temp <= threshold) setBg(coldBg)
+      else setBg(hotBg)
+    }
+
+    fetchWeatherData()
+    
+  }, [units, city])
+
+
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget
+    const currentUnit = button.innerText.slice()
+
+    const isFahrenheit = currentUnit === 'F'
+    
+    button.innerText = isFahrenheit ? "C" : "F"
+    setUnits(isFahrenheit ? "imperial" : "metric")
+  }
+
+  const enterKeyPressed = (e) => {
+    if(e.keyCode === 13) {
+      setCity(e.currentTarget.value)
+      e.currentTarget.blur()
+    }
+  }
+
   return (
-    <div className="App" style={{backgroundImage: `url(${coldBg})`}}>
+    <div className="App" style={{backgroundImage: `url(${bg})`}}>
       <div className='overlay'>
-        <div className='container'>
+        {weather && (
+          <div className='container'>
           <div className='section section_inputs'>
-            <input type='text' name='city' placeholder='Enter City...' />
-            <button>&deg;F</button>
+            <input onKeyDown={enterKeyPressed} type='text' name='city' placeholder='Enter City...' />
+            <button onClick={(e) => handleUnitsClick(e)}>&deg;C</button>
           </div>
           <div className='section section_temperature'>
             <div className='icon'>
-              <h3>London, GB</h3>
-              <img src='https://openweathermap.org/img/wn/02d@2x.png' alt='weather icon' />
-              <h3>Cloudy</h3>
+              <h3>{`${weather.name}, ${weather.country}`}</h3>
+              <img src={weather.iconURL} alt='weather icon' />
+              <h3>{weather.description}</h3>
             </div>
             <div className='temperature'>
-              <h1>34&deg;C</h1>
+              <h1>{`${weather.temp.toFixed()}`}&deg;{`${
+                units === "metric" ? "C" : "F"
+              }`}</h1>
             </div>
           </div>
-          <Descriptions />
+          <Descriptions weather={weather} units={units} />
         </div>
+        )}        
       </div>
     </div>
   );
