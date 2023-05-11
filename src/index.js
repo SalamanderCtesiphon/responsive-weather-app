a9c129f2259848d2da3172600150890c57b437e5
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-11 12:29:22 -0500

    defirebasing the app

--- src/index.js
+++ src/index.js
@@ -7,6 +7,4 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-const firebaseAppConfig = getFirebaseConfig();
-initializeApp(firebaseAppConfig);
 
