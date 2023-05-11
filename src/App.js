13d2ba09599f49dc3eed3f05527b779e2e3aa999
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-11 11:30:55 -0500

    added the firebase sdk

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import coldBg from './assest/cold.jpg'
 import Descriptions from './components/Descriptions';
 import { getFormattedWeatherData } from './weatherService';
 import { useEffect, useState } from 'react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNAT-NufAZWQKevwSBpbLo1HLWdzYm-0w",
+  authDomain: "weather-app-f672a.firebaseapp.com",
+  projectId: "weather-app-f672a",
+  storageBucket: "weather-app-f672a.appspot.com",
+  messagingSenderId: "268943571591",
+  appId: "1:268943571591:web:0be58ed5a121e8630e9622"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   const [city, setCity] = useState("Paris")
